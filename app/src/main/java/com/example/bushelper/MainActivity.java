accffcbcd8bacfd505b04ecfddbf764203214922
cyn8 <cynmsg@gmail.com>
2015-09-23 01:19:23 +0800

    增加点击按钮切换样式的逻辑

--- app/src/main/java/com/example/bushelper/MainActivity.java
+++ app/src/main/java/com/example/bushelper/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.bushelper;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -20,6 +23,8 @@ public class MainActivity extends Activity {
     private EditText etInit;    //起始地
     private EditText etDest;    //目的地
     private TextView tvHint;    //底部提示信息
+    private LinearLayout layoutLine;
+    private LinearLayout layoutTransfer;
 
 
     @Override
@@ -37,7 +42,39 @@ public class MainActivity extends Activity {
         etInit = (EditText) findViewById(R.id.activity_main_et_init);
         etDest = (EditText) findViewById(R.id.activity_main_et_dest);
         tvHint = (TextView) findViewById(R.id.activity_main_tv_hint);
+        layoutLine = (LinearLayout) findViewById(R.id.activity_main_layout_line);
+        layoutTransfer = (LinearLayout) findViewById(R.id.activity_main_layout_transfer);
+
+        //线路查询点击事件
+        btnLine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnTransfer.setBackgroundColor(Color.parseColor("#EEEEEE"));
+                btnTransfer.setTextColor(Color.parseColor("#000000"));
+                btnLine.setBackgroundColor(Color.parseColor("#3390D2"));
+                btnLine.setTextColor(Color.parseColor("#FFFFFF"));
+
+                layoutTransfer.setVisibility(View.GONE);
+                layoutLine.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //换乘查询点击事件
+        btnTransfer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnLine.setBackgroundColor(Color.parseColor("#EEEEEE"));
+                btnLine.setTextColor(Color.parseColor("#000000"));
+                btnTransfer.setBackgroundColor(Color.parseColor("#3390D2"));
+                btnTransfer.setTextColor(Color.parseColor("#FFFFFF"));
+
+                layoutLine.setVisibility(View.GONE);
+                layoutTransfer.setVisibility(View.VISIBLE);
+            }
+        });
     }
+
+
 
 
 }
